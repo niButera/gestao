27bfd7fb6c2f2c9e62b634c1e2212a833e0d925a
Nicolas Butera <126295707+niButera@users.noreply.github.com>
2024-05-24 20:52:04 -0300

    telas quase prontas

--- js/G-historico.js
+++ js/G-historico.js
@@ -10,7 +10,7 @@ function historico() {
             <h3>Quarto 01</h3>
             <h4>Informações</h4>
         </div>
-        <a href="historicoinfo.html"><img src="pics/SetaBlueGo.png" id="SetaBlueGo"></a>
+        <a href="G-Historico.html"><img src="pics/SetaBlueGo.png" id="SetaBlueGo"></a>
     </div>
 
     <div class="box" id="botao-adicional">
@@ -18,7 +18,7 @@ function historico() {
             <h3>Quarto 05</h3>
             <h4>Informações</h4>
         </div>
-        <a href="historicoinfo.html"><img src="pics/SetaBlueGo.png" id="SetaBlueGo"></a>
+        <a href="G-Historico.html"><img src="pics/SetaBlueGo.png" id="SetaBlueGo"></a>
     </div>
 
     <div class="box" id="botao-adicional">
@@ -26,7 +26,7 @@ function historico() {
             <h3>Quarto 10</h3>
             <h4>Informações</h4>
         </div>
-        <a href="historicoinfo.html"><img src="pics/SetaBlueGo.png" id="SetaBlueGo"></a>
+        <a href="G-Historico.html"><img src="pics/SetaBlueGo.png" id="SetaBlueGo"></a>
     </div>
         `;
         botoesContainer.insertAdjacentHTML('beforeend', botoesHTML);
