b3db1813da8ba14ceb7857d0efe5bf055d7c9e8f
niButera <nikolasxp683gmail.com>
2024-05-26 20:54:58 -0300

    hj programei feliz

--- js/G-Filtro.js
+++ js/G-Filtro.js
@@ -1,3 +1,23 @@
+// TELA FILTROS - BOTÃO QUARTOS
+function quartos() {
+    var botoesContainer = document.querySelector('.bloco1');
+    var botoesAdicionados = document.querySelectorAll('.botao-adicional');
+
+    if (botoesAdicionados.length === 0) {
+        var botoesHTML = `
+            <button class="btn3 botao-adicional">Leito 01</button>
+            <button class="btn3 botao-adicional">Leito 05</button>
+            <button class="btn3 botao-adicional">Leito 10</button>
+        `;
+        botoesContainer.insertAdjacentHTML('beforeend', botoesHTML);
+    } else {
+        botoesAdicionados.forEach(function (botao) {
+            botao.remove();
+        });
+    }
+}
+
+// BOTÃO CHAMADOS ANTECEDENTES
 document.getElementById("cmdA").addEventListener("click", function () {
     var info = document.getElementById("info");
     if (info.classList.contains("show")) {
